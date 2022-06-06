8f159f6b0970b4163c01a8dd20ae37167f0f339e
Bairamov Allaz <67458149+BairamovAllaz@users.noreply.github.com>
2022-06-06 20:56:03 +0300

    Update main.java

--- ForwardList/src/main.java
+++ ForwardList/src/main.java
@@ -4,15 +4,8 @@ import java.util.ListIterator;
 public class main {
 	public static void main(String args[]) { 
 		Linkedlist<String> a = new Linkedlist<String>("I","Am","Allaz","Bairamov");
-//		a.createLinkedlist("Hello","World","Fuck","You");
-//		a.createLinkedlist("Hello","World","Fuck","You");
-		
-		
-		
 		for(int i = 0; i < a.size();i++) { 
 			System.out.println(a.get(i));
 		}
-
-		
 	}
 }
